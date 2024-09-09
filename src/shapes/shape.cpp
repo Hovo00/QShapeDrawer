0979447f19696a1c9f26d0a86617049d6c660735
Hovo <hovik.grigoryan@picsart.com>
2024-09-09 12:25:17 +0400

    fix problem with drawing square and rectangle

--- src/shapes/shape.cpp
+++ src/shapes/shape.cpp
@@ -41,40 +41,12 @@ void Rectangle::draw(QPainter* painter) {
     painter->setBrush(Qt::blue);
     painter->setPen(Qt::blue);
 
-    // If only two points are provided, they are considered as diagonal points
-    if(shapeInfo.coordinates.size() == 2) {
-        QPoint topLeft = shapeInfo.coordinates[0].toPoint();
-        QPoint bottomRight = shapeInfo.coordinates[1].toPoint();
-        QRect rect(topLeft, bottomRight - topLeft);
+    if (shapeInfo.coordinates.size() == 2) {
+        QPointF topLeft = shapeInfo.coordinates[0];
+        QPointF bottomRight = shapeInfo.coordinates[1];
+        QRect rect(topLeft.toPoint(), QSize(bottomRight.x() - topLeft.x(), bottomRight.y() - topLeft.y()));
         painter->drawRect(rect);
-    }
-    // If four points are provided, they are considered as the four corners
-    else if(shapeInfo.coordinates.size() == 4) {
-        int topLeftX = std::min({shapeInfo.coordinates[0].x(), shapeInfo.coordinates[1].x(), shapeInfo.coordinates[2].x(), shapeInfo.coordinates[3].x()});
-        int topLeftY = std::min({shapeInfo.coordinates[0].y(), shapeInfo.coordinates[1].y(), shapeInfo.coordinates[2].y(), shapeInfo.coordinates[3].y()});
-        int bottomRightX = std::max({shapeInfo.coordinates[0].x(), shapeInfo.coordinates[1].x(), shapeInfo.coordinates[2].x(), shapeInfo.coordinates[3].x()});
-        int bottomRightY = std::max({shapeInfo.coordinates[0].y(), shapeInfo.coordinates[1].y(), shapeInfo.coordinates[2].y(), shapeInfo.coordinates[3].y()});
-        QRect rect(topLeftX, topLeftY, bottomRightX - topLeftX, bottomRightY - topLeftY);
-        painter->drawRect(rect);
-    }
-}
-
-
-Square::Square(const ShapeInfo &info) : Shape(info) {}
-
-void Square::draw(QPainter* painter) {
-    painter->setBrush(Qt::blue);
-    painter->setPen(Qt::blue);
-
-    // If only two points are provided, they are considered as diagonal points
-    if(shapeInfo.coordinates.size() == 2) {
-        QPoint topLeft = shapeInfo.coordinates[0].toPoint();
-        QPoint bottomRight = shapeInfo.coordinates[1].toPoint();
-        QRect rect(topLeft, bottomRight - topLeft);
-        painter->drawRect(rect);
-    }
-    // If four points are provided, they are considered as the four corners
-    else if(shapeInfo.coordinates.size() == 4) {
+    } else if (shapeInfo.coordinates.size() == 4) {
         int topLeftX = std::min({shapeInfo.coordinates[0].x(), shapeInfo.coordinates[1].x(), shapeInfo.coordinates[2].x(), shapeInfo.coordinates[3].x()});
         int topLeftY = std::min({shapeInfo.coordinates[0].y(), shapeInfo.coordinates[1].y(), shapeInfo.coordinates[2].y(), shapeInfo.coordinates[3].y()});
         int bottomRightX = std::max({shapeInfo.coordinates[0].x(), shapeInfo.coordinates[1].x(), shapeInfo.coordinates[2].x(), shapeInfo.coordinates[3].x()});
@@ -94,6 +66,29 @@ QPointF Rectangle::center() const {
         return (topLeft + bottomRight) / 2;
     }
     return QPointF();
+}
+
+Square::Square(const ShapeInfo &info) : Shape(info) {}
+
+void Square::draw(QPainter* painter) {
+    painter->setBrush(Qt::blue);
+    painter->setPen(Qt::blue);
+
+    if (shapeInfo.coordinates.size() == 2) {
+        QPointF topLeft = shapeInfo.coordinates[0];
+        QPointF bottomRight = shapeInfo.coordinates[1];
+        int sideLength = qMin(qAbs(bottomRight.x() - topLeft.x()), qAbs(bottomRight.y() - topLeft.y()));
+        QRect rect(topLeft.toPoint(), QSize(sideLength, sideLength));
+        painter->drawRect(rect);
+    } else if (shapeInfo.coordinates.size() == 4) {
+        int topLeftX = std::min({shapeInfo.coordinates[0].x(), shapeInfo.coordinates[1].x(), shapeInfo.coordinates[2].x(), shapeInfo.coordinates[3].x()});
+        int topLeftY = std::min({shapeInfo.coordinates[0].y(), shapeInfo.coordinates[1].y(), shapeInfo.coordinates[2].y(), shapeInfo.coordinates[3].y()});
+        int bottomRightX = std::max({shapeInfo.coordinates[0].x(), shapeInfo.coordinates[1].x(), shapeInfo.coordinates[2].x(), shapeInfo.coordinates[3].x()});
+        int bottomRightY = std::max({shapeInfo.coordinates[0].y(), shapeInfo.coordinates[1].y(), shapeInfo.coordinates[2].y(), shapeInfo.coordinates[3].y()});
+        int sideLength = qMin(bottomRightX - topLeftX, bottomRightY - topLeftY);
+        QRect rect(topLeftX, topLeftY, sideLength, sideLength);
+        painter->drawRect(rect);
+    }
 }
 
 QPointF Square::center() const {
